420caf13ee844648e41f6df095f56b1d6122f756
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2021-09-18 13:56:59 +0530

    feat: historic A

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -1,3 +1,4 @@
+import brownie
 from brownie._config import CONFIG
 
 
@@ -5,3 +6,13 @@ def configure_network(node_provider_https: str, network_name: str = "mainnet") -
     # change network provider to user specified
     CONFIG.networks[network_name]["host"] = node_provider_https
     CONFIG.networks[network_name]["name"] = "Ethereum mainnet"
+
+
+def configure_network_and_connect(
+    node_provider_https: str, network_name: str = "mainnet"
+) -> None:
+    # change network provider to user specified
+    CONFIG.networks[network_name]["host"] = node_provider_https
+    CONFIG.networks[network_name]["name"] = "Ethereum mainnet"
+
+    brownie.network.connect(network_name)
