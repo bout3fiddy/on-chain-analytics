37744cb03abd1e745263de73a25733d52046fcd2
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2021-09-19 18:44:36 +0530

    fix: admin addr for ramping factory pool

--- utils/contract_utils.py
+++ utils/contract_utils.py
@@ -6,3 +6,19 @@ def init_contract(contract_addr: str):
         return Contract(contract_addr)
     except:
         return Contract.from_explorer(contract_addr)
+
+
+def main():
+    import brownie
+
+    brownie.network.connect("mainnet")
+
+    pool_contract_addr = "0x5a6A4D54456819380173272A5E8E9B9904BdF41B"  # mim-3crv pool
+    pool_contract = init_contract(pool_contract_addr)
+    print(pool_contract.info(), pool_contract.name())
+
+    brownie.network.disconnect()
+
+
+if __name__ == "__main__":
+    main()
