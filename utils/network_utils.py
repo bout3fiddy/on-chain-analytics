5338691c7a85635b875ac5dada3ccfaee28853b0
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2021-11-07 17:22:40 +0100

    wip

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -1,5 +1,9 @@
+import os
+
 import brownie
 from brownie._config import CONFIG
+
+ALCHEMY_API_KEY = os.environ['ALCHEMY_API_KEY']
 
 
 def configure_network(node_provider_https: str, network_name: str = "mainnet") -> None:
@@ -16,3 +20,11 @@ def configure_network_and_connect(
     CONFIG.networks[network_name]["name"] = "Ethereum mainnet"
 
     brownie.network.connect(network_name)
+
+
+def connect_eth_alchemy():
+    if not brownie.network.is_connected():
+        configure_network_and_connect(
+            node_provider_https=f"https://eth-mainnet.alchemyapi.io/v2/{ALCHEMY_API_KEY}",
+            network_name='mainnet'
+        )
