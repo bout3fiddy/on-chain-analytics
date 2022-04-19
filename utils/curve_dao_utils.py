b5155b24fd838d89b2bbbd00888f84312d096be8
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-04-19 11:44:58 +0200

    feat: dao utils

---
+++ utils/curve_dao_utils.py
@@ -0,0 +1,146 @@
+import json
+import warnings
+
+import requests
+
+from brownie import Contract, accounts, chain, network
+from brownie.convert import to_address
+
+warnings.filterwarnings("ignore")
+
+
+# ------- CONSTANTS --------- #
+
+CURVE_DAO_OWNERSHIP = {
+    "agent": "0x40907540d8a6c65c637785e8f8b742ae6b0b9968",
+    "voting": "0xe478de485ad2fe566d49342cbd03e49ed7db3356",
+    "token": "0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2",
+    "quorum": 30,
+}
+
+CURVE_DAO_PARAM = {
+    "agent": "0x4eeb3ba4f221ca16ed4a0cc7254e2e32df948c5f",
+    "voting": "0xbcff8b0b9419b9a88c44546519b1e909cf330399",
+    "token": "0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2",
+    "quorum": 15,
+}
+
+EMERGENCY_DAO = {
+    "forwarder": "0xf409Ce40B5bb1e4Ef8e97b1979629859c6d5481f",
+    "agent": "0x00669DF67E4827FCc0E48A1838a8d5AB79281909",
+    "voting": "0x1115c9b3168563354137cdc60efb66552dd50678",
+    "token": "0x4c0947B16FB1f755A2D32EC21A0c4181f711C500",
+    "quorum": 51,
+}
+
+
+# ------- DAO VOTE SIM SCRIPTS --------- #
+
+
+def prepare_evm_script(target: dict, actions: dict) -> str:
+    """Generates EVM script to be executed by AragonDAO contracts.
+
+    Args:
+        target (dict): one of either CURVE_DAO_OWNERSHIP, CURVE_DAO_PARAMS or EMERGENCY_DAO
+        actions (list(tuple)): ("target addr", "fn_name", *args)
+
+    Returns:
+        str: Generated EVM script.
+    """
+    agent = Contract(target["agent"])
+    evm_script = "0x00000001"
+
+    for address, fn_name, *args in actions:
+        contract = Contract(address)
+        fn = getattr(contract, fn_name)
+        calldata = fn.encode_input(*args)
+        agent_calldata = agent.execute.encode_input(address, 0, calldata)[2:]
+        length = hex(len(agent_calldata) // 2)[2:].zfill(8)
+        evm_script = f"{evm_script}{agent.address[2:]}{length}{agent_calldata}"
+
+    return evm_script
+
+
+def make_vote(sender: str, target: dict, actions: list(tuple), description: str) -> str:
+    """Prepares EVM script and creates an on-chain AragonDAO vote.
+
+    Note: this script can be used to deploy on-chain governance proposals as well.
+
+    Args:
+        sender (str): msg.sender address
+        target (dict): one of either CURVE_DAO_OWNERSHIP, CURVE_DAO_PARAMS or EMERGENCY_DAO
+        actions (list(tuple)): ("target addr", "fn_name", *args)
+        description (str): Description of the on-chain governance proposal
+
+    Returns:
+        str: vote ID of the created vote.
+    """
+    if network.show_active() == "mainnet":
+        kw = {"from": sender, "priority_fee": "2 gwei"}
+    else:
+        kw = {"from": sender}
+    text = json.dumps({"text": description})
+    response = requests.post(
+        "https://ipfs.infura.io:5001/api/v0/add", files={"file": text}
+    )
+    ipfs_hash = response.json()["Hash"]
+    print(f"ipfs hash: {ipfs_hash}")
+
+    aragon = Contract(target["voting"])
+    evm_script = prepare_evm_script(target, actions)
+    if target.get("forwarder"):
+        # the emergency DAO only allows new votes via a forwarder contract
+        # so we have to wrap the call in another layer of evm script
+        vote_calldata = aragon.newVote.encode_input(
+            evm_script, description, False, False
+        )[2:]
+        length = hex(len(vote_calldata) // 2)[2:].zfill(8)
+        evm_script = f"0x00000001{aragon.address[2:]}{length}{vote_calldata}"
+        print(f"Target: {target['forwarder']}\nEVM script: {evm_script}")
+        tx = Contract(target["forwarder"]).forward(evm_script, kw)
+    else:
+        print(f"Target: {aragon.address}\nEVM script: {evm_script}")
+        tx = aragon.newVote(evm_script, f"ipfs:{ipfs_hash}", False, False, kw)
+
+    vote_id = tx.events["StartVote"]["voteId"]
+
+    print(f"\nSuccess! Vote ID: {vote_id}")
+    return vote_id
+
+
+def simulate(target: dict, actions: dict, description: str):
+    """Create AragonDAO vote and simulate passing vote on mainnet-fork.
+
+    Args:
+        target (dict): one of either CURVE_DAO_OWNERSHIP, CURVE_DAO_PARAMS or EMERGENCY_DAO
+        actions (list(tuple)): ("target addr", "fn_name", *args)
+        description (str): Description of the on-chain governance proposal
+    """
+    # fetch the top holders so we can pass the vote
+    data = requests.get(
+        f"https://api.ethplorer.io/getTopTokenHolders/{target['token']}",
+        params={"apiKey": "freekey", "limit": 50},
+    ).json()["holders"][::-1]
+
+    # create a list of top holders that will be sufficient to make quorum
+    holders = []
+    weight = 0
+    while weight < target["quorum"] + 5:
+        row = data.pop()
+        holders.append(to_address(row["address"]))
+        weight += row["share"]
+
+    # make the new vote
+    top_holder = holders[0]
+    vote_id = make_vote(top_holder, target, actions, description)
+
+    # vote
+    aragon = Contract(target["voting"])
+    for acct in holders:
+        aragon.vote(vote_id, True, False, {"from": acct})
+
+    # sleep for a week so it has time to pass
+    chain.sleep(86400 * 7)
+
+    # moment of truth - execute the vote!
+    aragon.executeVote(vote_id, {"from": top_holder})
