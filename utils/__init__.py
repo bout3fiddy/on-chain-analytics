bd12b9d13b79f84e254668af69841f1378a5a62c
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-04-20 00:37:38 +0200

    feat: simulate bean:3crv gauge culling

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,5 +2,5 @@ from .contract_utils import init_contract
 from .network_utils import configure_network
 
 
-from .curve_dao_utils import CURVE_DAO_OWNERSHIP
+from .curve_dao_utils import CURVE_DAO_OWNERSHIP, CURVE_DAO_PARAM
 from .curve_dao_utils import simulate as simulate_curve_dao_vote
